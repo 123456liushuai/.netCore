1552366156558b3627e11fd8dc8ca77d8dc8f0be
liushuai <2812895672@qq.com>
2024-05-16 15:35:03 +0800

    主页设计

---
+++ front/src/router/index.ts
@@ -0,0 +1,9 @@
+import {createRouter,createWebHistory} from 'vue-router'import LoginPage from "../view/index/LoginPage.vue"
+import RootPage from "../view/index/RootPage.vue"
+import DeskTop from '../view/index/DeskTop.vue'const router = createRouter({  history: createWebHistory(),  routes: [
+	 { path: '/', component: RootPage ,
+	 children: [
+	     { name: "工作台", path: "/desktop", component: DeskTop },
+	 ]
+	 },
+	 { path: '/login', component: LoginPage }  ]})export default router
